64d8a8a40c80dd413a0d5de405d026bbf1835d3f
liuyp2080 <78184640+liuyp2080@users.noreply.github.com>
2024-08-02 09:39:04 +0800

    Add files via upload

--- pages/1🔎select_the_variables.py
+++ pages/1🔎select_the_variables.py
@@ -18,8 +18,55 @@ from lightgbm import LGBMRegressor, LGBMClassifier
 import matplotlib.pyplot as plt
 from arfs.preprocessing import OrdinalEncoderPandas
 
+#--------------------------------------
+import hmac
+import streamlit as st
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
 
 if __name__ == '__main__':
+    
+    if not check_password():
+        st.stop()
+
+    # Main Streamlit app starts here
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
 
     st.title("🔎变量筛选")
     model_type=st.radio('模型类别选择', ('回归', '二分类'),horizontal=True)
